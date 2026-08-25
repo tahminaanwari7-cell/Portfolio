a7bce64ee71080f110b19d56770c3dfdf0f9cd5d
tahminaanwari7-cell <tahminaanwari7@gmail.com>
2026-08-25 22:46:25 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const faqItems = document.querySelectorAll(".faq-item");
 
-faqItems.forEach((item) => {
-
-  item.addEventListener("mouseenter", () => {
-
-    // Close all FAQ items
-    faqItems.forEach((faq) => {
-      faq.classList.remove("active");
-    });
-
-    // Open the item under the cursor
-    item.classList.add("active");
-
-  });
-
-});
